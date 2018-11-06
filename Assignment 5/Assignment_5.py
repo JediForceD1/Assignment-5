085e9b54c1e1eddc313f3dceb8ef6728fe8a8447
JediForceD1 <44473978+JediForceD1@users.noreply.github.com>
2018-11-06 11:26:45 -0600

    upload

--- Assignment 5/Assignment_5.py
+++ Assignment 5/Assignment_5.py
@@ -1,5 +1,9 @@
+'''
+Aaron McCormick
+Assingment 5
+10/18/2018
+'''
 def main():
-    # Setup variables. DO NOT changes lines 3-9
     lst_corr_ans = ["A", "C", "A", "A", "D", "B",
 			"C", "A", "C", "B", "A", "D",
 			"C", "A", "D", "C", "B", "B",
@@ -7,26 +11,25 @@ def main():
     lst_user_answers = []
     corr_count = 0
     num_questions = 20
-
-    # LOOP 1 : Setup file variable and open the user_answers.txt file
-    # Place statements to load lst_user_answers from records in user_answers.txt
-    # file. Remember to rstrip the \n character
-    
     print("Q\tcorr\tYour\tStatus")
     print("#\tAnswer\tAnswer\n--------------------------")
-
-    # LOOP 2 : Code for loop to process each element from lst_corr_ans  and
-    # lst_user_answers and update corr_count counter variable accordingly
-
+    with open("user_answers.txt", 'r') as f:
+        lst_user_answers = [line.rstrip("\n") for line in f]   # save chars to a list
+    for i, item in enumerate(lst_user_answers):
+        lst_user_answers[i] = item.rstrip("\r")
+    pos = int(0)
+    for i in lst_corr_ans:
+        if i == lst_user_answers[pos]:
+            print(pos + 1, "\t", lst_corr_ans[pos], "\t", lst_user_answers[pos], "\t", sep = "")
+            corr_count += 1         
+        elif i != lst_user_answers[pos]:
+             print(pos + 1, "\t", lst_corr_ans[pos], "\t", lst_user_answers[pos], "\tX", sep = "")
+        pos += 1
     percent_corr = (corr_count/20) * 100
     percent_corr_fmt = format(percent_corr, ".1f")
-    print("\nGrade : ", corr_count , "/", num_questions, " = ",
-         percent_corr_fmt, sep="")
-    # Determine if student passed and display result
+    print("\nGrade : ", corr_count , "/", num_questions, " = ", percent_corr_fmt, sep="")
     if percent_corr >= 75:
         print("Congratulations!! You passed the exam")
     else:
         print("Sorry, you did not pass the exam")
-   # Close file
-
 main()
